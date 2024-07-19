67b9b8f8e2b6d22db64e809acb8b6a5155b6ccfa
itsamon2 <itsamon2@gmail.com>
2024-07-19 22:49:58 +0300

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+      cropName: 'Maize',
+      datePlanted: '2-3-2024',
+      growthStage: 'Reproductive Stage',
+      condition: 'Good'
+  },
+  {
+    cropName: 'Wheat',
+    datePlanted: '1-2-2024',
+    growthStage: 'Maturation',
+    condition: 'Perfect'
+  },
+  {
+    cropName: 'Barley',
+    datePlanted: '3-1-2024',
+    growthStage: 'Maturation',
+    condition: 'Perfect'
+  },
+]
